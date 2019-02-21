0d880a66f05beb1e1df0d999cd4e93e6e7f8cd97
ljingya <ljya0805@163.com>
2019-02-21 22:33:50 +0800

    添加ignore文件

--- app/src/main/java/com/lijingya/androidknowledge/chapter01/service/ServiceKg.java
+++ app/src/main/java/com/lijingya/androidknowledge/chapter01/service/ServiceKg.java
@@ -1,42 +1,76 @@
 package com.lijingya.androidknowledge.chapter01.service;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
+import com.lijingya.androidknowledge.MainActivity;
+import com.lijingya.androidknowledge.R;
 
 public class ServiceKg extends Service {
 
-    private String TAG=getClass().getSimpleName();
+    private String TAG = getClass().getSimpleName();
+
     @Override
     public IBinder onBind(Intent intent) {
-        Log.d(TAG,"onBind");
+        Log.d(TAG, "onBind");
         return null;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d(TAG,"onCreate");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "chat";
+            String channelName = "聊天消息";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            createNotificationChannel(channelId, channelName, importance);
 
+        }
+
+
+        Log.d(TAG, "onCreate");
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(this, "chat").setContentTitle("收到一条聊天消息").setContentText("今天中午吃什么？").setWhen(System.currentTimeMillis()).setSmallIcon(R.mipmap.ic_launcher).setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher)).setAutoCancel(true).build();
+
+
+        startForeground(1, notification);
 
     }
 
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createNotificationChannel(String channelId, String channelName, int importance) {
+        NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG,"onStartCommand");
+        Log.d(TAG, "onStartCommand");
+
         return Service.START_STICKY;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        Log.d(TAG,"onUnbind");
+        Log.d(TAG, "onUnbind");
         return super.onUnbind(intent);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d(TAG,"onDestroy");
+        Log.d(TAG, "onDestroy");
     }
 }
